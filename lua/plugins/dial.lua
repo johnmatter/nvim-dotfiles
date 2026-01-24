26cbe8e606463c383a4b97c1d720908532917725
John Matter <jmatter4@gmail.com>
2026-01-23 23:49:38 -0500

    update

--- lua/plugins/dial.lua
+++ lua/plugins/dial.lua
@@ -12,11 +12,11 @@ return {
       end, 20)
     end
 
-    vim.keymap.set("n", "<F12>", function()
+    vim.keymap.set("n", "<F4>", function()
       feed_and_save("<Plug>(dial-increment)")
     end, { noremap = true })
 
-    vim.keymap.set("n", "<F11>", function()
+    vim.keymap.set("n", "<F3>", function()
       feed_and_save("<Plug>(dial-decrement)")
     end, { noremap = true })
 
